aeb5b14f1d7d79629d7aed8ebdce075ef885a788
Arshya <codingbyarshya@gmail.com>
2023-04-05 00:47:16 +0300

    exercise34.js

---
+++ exercise34.js
@@ -0,0 +1,13 @@
+const printHello = () => console.log("Hello");
+
+function printAsyncName(callback, name) {
+  setTimeout(() => callback(), 1000);
+  setTimeout(() => console.log(name), 2000);
+}
+
+
+
+
+
+
+printAsyncName(printHello, "Arshya");
